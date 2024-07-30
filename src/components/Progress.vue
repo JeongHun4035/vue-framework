ea584a21e950a69acdb7d31dcec02584b632c20c
Orion <eogns4035@gmail.com>
2024-07-30 21:36:57 +0900

    progress Event fix

--- src/components/Progress.vue
+++ src/components/Progress.vue
@@ -7,17 +7,44 @@ withDefaults(
     percentage: 0,
   }
 )
+
+const loadingText = ref('Loading...'.split(''))
+
+function getAnimationDelay(index: number) {
+  return { animationDelay: `${index * 0.1}s` }
+}
 </script>
 
 <template>
   <div class="overlay">
-    <div class="progress-bar">
-      <div class="progress" :style="{ width: percentage + '%' }"></div>
+    <div class="progress-area">
+      <div class="loading-text">
+        <span
+          v-for="(char, index) in loadingText"
+          :key="index"
+          :style="getAnimationDelay(index)"
+        >
+          {{ char }}
+        </span>
+      </div>
+      <div class="progress-bar">
+        <div class="progress" :style="{ width: percentage + '%' }"></div>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+@keyframes wobble {
+  0%,
+  100% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-5px);
+  }
+}
+
 .overlay {
   position: fixed;
   top: 0;
@@ -31,8 +58,21 @@ withDefaults(
   z-index: 1000;
 }
 
+.progress-area {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  color: #ffffff;
+  align-items: center;
+}
+
+.loading-text span {
+  display: inline-block;
+  animation: wobble 1s infinite;
+}
+
 .progress-bar {
-  width: 80%;
+  width: 100%;
   height: 10px;
   background: #ccc;
   border-radius: 5px;
